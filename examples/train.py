05b271fc06ed0295051594b7c243e4054d8930d2
Stone <zyshi21@cse.cuhk.edu.hk>
2023-10-12 10:51:28 +0800

    update

--- examples/train.py
+++ examples/train.py
@@ -21,11 +21,11 @@ if __name__ == '__main__':
     model = deepgate.Model()
     
     trainer = deepgate.Trainer(model, distributed=True)
-    trainer.set_training_args(prob_rc_func_weight=[1.0, 0.0, 0.0], lr=1e-4, lr_step=30)
+    trainer.set_training_args(prob_rc_func_weight=[3.0, 1.0, 0.0], lr=1e-4, lr_step=50)
     print('[INFO] Stage 1 Training ...')
     trainer.train(num_epochs, train_dataset, val_dataset)
     
     print('[INFO] Stage 2 Training ...')
-    trainer.set_training_args(prob_rc_func_weight=[3.0, 1.0, 2.0], lr=1e-4, lr_step=30)
+    trainer.set_training_args(prob_rc_func_weight=[3.0, 1.0, 2.0], lr=1e-4, lr_step=50)
     trainer.train(num_epochs, train_dataset, val_dataset)
     
